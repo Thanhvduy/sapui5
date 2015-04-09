57b324ceef581d0cdd63b541279bd0ee67b1f564
Thanhvduy <vuduythanh1989@gmail.com>
2015-04-09 18:13:29 +0700

    update 2

--- XML_ProgressIndicator/WebContent/xml_progressindicator/ProgressIndicator.controller.js
+++ XML_ProgressIndicator/WebContent/xml_progressindicator/ProgressIndicator.controller.js
@@ -1,38 +1,46 @@
 sap.ui.controller("sap.m.sample.xml_progressindicator.ProgressIndicator", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf xml_progressindicator.ProgressIndicator
-*/
-	onInit: function() {
-
+	/**
+	 * Called when a controller is instantiated and its View controls (if
+	 * available) are already created. Can be used to modify the View before it
+	 * is displayed, to bind event handlers and do other one-time
+	 * initialization.
+	 * 
+	 * @memberOf xml_progressindicator.ProgressIndicator
+	 */
+	onInit : function() {
+		var oModel = new sap.ui.model.json.JSONModel("model/products.json");
+		this.getView().setModel(oModel);
+		
 	},
 
 /**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf xml_progressindicator.ProgressIndicator
-*/
-//	onBeforeRendering: function() {
+ * Similar to onAfterRendering, but this hook is invoked before the controller's
+ * View is re-rendered (NOT before the first rendering! onInit() is used for
+ * that one!).
+ * 
+ * @memberOf xml_progressindicator.ProgressIndicator
+ */
+// onBeforeRendering: function() {
 //
-//	},
-
+// },
 /**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf xml_progressindicator.ProgressIndicator
-*/
-//	onAfterRendering: function() {
+ * Called when the View has been rendered (so its HTML is part of the document).
+ * Post-rendering manipulations of the HTML could be done here. This hook is the
+ * same one that SAPUI5 controls get after being rendered.
+ * 
+ * @memberOf xml_progressindicator.ProgressIndicator
+ */
+// onAfterRendering: function() {
 //
-//	},
-
+// },
 /**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf xml_progressindicator.ProgressIndicator
-*/
-//	onExit: function() {
+ * Called when the Controller is destroyed. Use this one to free resources and
+ * finalize activities.
+ * 
+ * @memberOf xml_progressindicator.ProgressIndicator
+ */
+// onExit: function() {
 //
-//	}
-
+// }
 });
